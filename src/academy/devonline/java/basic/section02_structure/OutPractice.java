bc200471121b10d49f33eb583e8a0f4d822fc481
devonline <devonline.academy@gmail.com>
2019-06-18 18:33:06 +0300

    OutPractice

---
+++ src/academy/devonline/java/basic/section02_structure/OutPractice.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2019. http://devonline.academy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package academy.devonline.java.basic.section02_structure;
+
+/**
+ * @author devonline
+ * @link http://devonline.academy/java-basic
+ */
+public class OutPractice {
+    public static void main(String[] args) {
+        System.out.print(0);
+        System.out.print(3);
+        System.out.print(6);
+        System.out.print(9);
+
+        System.out.println();
+
+        System.out.println(0);
+        System.out.println(3);
+        System.out.println(6);
+        System.out.println(9);
+    }
+}
