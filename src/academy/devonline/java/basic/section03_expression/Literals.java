b686f64c57cfa8bc399947bce9b3c98e12638834
devonline <devonline.academy@gmail.com>
2019-06-19 10:38:33 +0300

    Literals

---
+++ src/academy/devonline/java/basic/section03_expression/Literals.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2019. http://devonline.academy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package academy.devonline.java.basic.section03_expression;
+
+/**
+ * @author devonline
+ * @link http://devonline.academy/java-basic
+ */
+public class Literals {
+    public static void main(String[] args) {
+        // int
+        var i = 1234;
+        // double
+        var d1 = 1.;
+        var d2 = 1.0;
+        var d3 = 1.123456;
+        var d4 = 5e3;// 5 * 10 ^ 3 == 5000.0
+        // boolean
+        var b1 = true;
+        var b2 = false;
+        // char
+        var ch1 = 'a';
+        var ch2 = '\t';
+        var ch3 = '\n';
+        // String
+        var s1 = "a";
+        var s2 = "Hello world";
+
+        //
+        int num = 1;
+        char ch = '1';
+        String s = "1";
+    }
+}
