0e38a20645824dda5b523886a3db63436a3f85aa
devonline <devonline.academy@gmail.com>
2019-06-19 19:49:41 +0300

    GuessTheNumber (loop version)

---
+++ src/academy/devonline/java/basic/section05_cycles/GuessTheNumber.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2019. http://devonline.academy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package academy.devonline.java.basic.section05_cycles;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * @author devonline
+ * @link http://devonline.academy/java-basic
+ */
+public class GuessTheNumber {
+    public static void main(String[] args) {
+        var number = new Random().nextInt(10);
+
+        while (true) {
+            var userCase = new Scanner(System.in).nextInt();
+            if (number > userCase) {
+                System.out.println("number > " + userCase + ". Try again: ");
+            } else if (number < userCase) {
+                System.out.println("number < " + userCase + ". Try again: ");
+            } else {
+                System.out.println("Congratulations, you guessed the number!");
+                break;
+            }
+        }
+    }
+}
