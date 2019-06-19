3059fc7bc2659ce0d8c3cec4240be501fc629840
devonline <devonline.academy@gmail.com>
2019-06-19 15:54:12 +0300

    WeekDayWithSwitch

---
+++ src/academy/devonline/java/basic/section04_conditional/WeekDayWithSwitch.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2019. http://devonline.academy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package academy.devonline.java.basic.section04_conditional;
+
+/**
+ * @author devonline
+ * @link http://devonline.academy/java-basic
+ */
+public class WeekDayWithSwitch {
+    public static void main(String[] args) {
+        var day = 7;
+
+        switch (day){
+            case 1: {
+                System.out.println("Monday");
+                break;
+            }
+            case 2: {
+                System.out.println("Tuesday");
+                break;
+            }
+            case 3: {
+                System.out.println("Wednesday");
+                break;
+            }
+            case 4: {
+                System.out.println("Thursday");
+                break;
+            }
+            case 5: {
+                System.out.println("Friday");
+                break;
+            }
+            case 6: {
+                System.out.println("Saturday");
+                break;
+            }
+            case 7: {
+                System.out.println("Sunday");
+                break;
+            }
+            default:{
+                System.out.println("Invalid day: "+day);
+            }
+        }
+    }
+}
