fd9403a510e2cf75800bf54ebed3f72acbef4cf4
devonline <devonline.academy@gmail.com>
2019-06-19 14:59:15 +0300

    WeekDay

---
+++ src/academy/devonline/java/basic/section04_conditional/WeekDay.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2019. http://devonline.academy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package academy.devonline.java.basic.section04_conditional;
+
+/**
+ * @author devonline
+ * @link http://devonline.academy/java-basic
+ */
+public class WeekDay {
+    public static void main(String[] args) {
+        var day = 1;
+
+        if (day == 1) {
+            System.out.println("Monday");
+        } else if (day == 2) {
+            System.out.println("Tuesday");
+        } else if (day == 3) {
+            System.out.println("Wednesday");
+        } else if (day == 4) {
+            System.out.println("Thursday");
+        } else if (day == 5) {
+            System.out.println("Friday");
+        } else if (day == 6) {
+            System.out.println("Saturday");
+        } else if (day == 7) {
+            System.out.println("Sunday");
+        } else {
+            System.out.println("Invalid day: " + day);
+        }
+    }
+}
