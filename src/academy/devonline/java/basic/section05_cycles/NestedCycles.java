c4262e0a9af5905a856b01cd04e83e216f0f4f5a
devonline <devonline.academy@gmail.com>
2019-06-19 20:26:27 +0300

    NestedCycles

---
+++ src/academy/devonline/java/basic/section05_cycles/NestedCycles.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2019. http://devonline.academy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package academy.devonline.java.basic.section05_cycles;
+
+/**
+ * @author devonline
+ * @link http://devonline.academy/java-basic
+ */
+public class NestedCycles {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print((i * 10 + j) + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
