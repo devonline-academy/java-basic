5a9061a6e537d230ccb3a74ae6479a2d5f181f40
devonline <devonline.academy@gmail.com>
2019-06-19 14:51:12 +0300

    SimpleCalculator

---
+++ src/academy/devonline/java/basic/section04_conditional/SimpleCalculator.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2019. http://devonline.academy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package academy.devonline.java.basic.section04_conditional;
+
+import java.util.Scanner;
+
+/**
+ * @author devonline
+ * @link http://devonline.academy/java-basic
+ */
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        System.out.println("Enter 'a':");
+        var a = new Scanner(System.in).nextInt();
+        System.out.println("Enter 'b':");
+        var b = new Scanner(System.in).nextInt();
+        System.out.println("Enter operator: {+,-, *,/,%}");
+        var operator = new Scanner(System.in).nextLine().charAt(0);
+
+        if (operator == '+') {
+            System.out.println("a + b = " + (a + b));
+        } else if (operator == '-') {
+            System.out.println("a - b = " + (a - b));
+        } else if (operator == '*') {
+            System.out.println("a * b = " + (a * b));
+        } else if (operator == '/') {
+            System.out.println("a / b = " + (a / b));
+        } else if (operator == '%') {
+            System.out.println("a % b = " + (a % b));
+        } else {
+            System.out.println("Unsupported operator: " + operator);
+        }
+    }
+}
