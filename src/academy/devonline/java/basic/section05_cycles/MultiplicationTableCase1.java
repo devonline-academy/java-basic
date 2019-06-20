4de3a15fed2e80f5d43e309952675d409083baf7
devonline <devonline.academy@gmail.com>
2019-06-20 11:08:20 +0300

    MultiplicationTableCase1

--- src/academy/devonline/java/basic/section05_cycles/MultiplicationTableCase1.java
+++ src/academy/devonline/java/basic/section05_cycles/MultiplicationTableCase1.java
@@ -23,14 +23,9 @@ package academy.devonline.java.basic.section05_cycles;
 public class MultiplicationTableCase1 {
     public static void main(String[] args) {
         for (int i = 1; i < 10; i++) {
-            System.out.print((i == 1 ? "" : i) + "\t");
-        }
-        System.out.println();
-
-        for (int i = 2; i < 10; i++) {
-            System.out.print(i + "\t");
-            for (int j = 2; j < 10; j++) {
-                System.out.print(i * j + "\t");
+            for (int j = 1; j < 10; j++) {
+                var res = i * j;
+                System.out.print((res == 1 ? "" : res) + "\t");
             }
             System.out.println();
         }
