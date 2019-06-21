1df452c5dc432ec664bfd1dd90af1dbddc2eb287
devonline <devonline.academy@gmail.com>
2019-06-21 17:54:57 +0300

    NewArrayWithSize

--- src/academy/devonline/java/basic/section06_array/NewArray.java
+++ src/academy/devonline/java/basic/section06_array/NewArray.java
@@ -28,6 +28,10 @@ public class NewArray {
         char[] arrayWithValues4 = {'1', '2', '3', '4', '5'};
         String[] arrayWithValues5 = {"1", "2", "3", "4", "5"};
 
-
+        int[] arrayWithSize1 = new int[5]; // 0
+        double[] arrayWithSize2 = new double[5]; // 0.0
+        boolean[] arrayWithSize3 = new boolean[5]; // false
+        char[] arrayWithSize4 = new char[5]; // \u0000
+        String[] arrayWithSize5 = new String[5]; // null
     }
 }
