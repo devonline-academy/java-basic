502d4b66aa501359ddfa3253d1c3c28cb03b825d
devonline <devonline.academy@gmail.com>
2019-06-21 20:44:38 +0300

    skills

--- src/academy/devonline/java/basic/section05_cycles/MultiplicationTableCase3.java
+++ src/academy/devonline/java/basic/section05_cycles/MultiplicationTableCase3.java
@@ -22,17 +22,17 @@ package academy.devonline.java.basic.section05_cycles;
  */
 public class MultiplicationTableCase3 {
     public static void main(String[] args) {
+        // read source data
+        var to = 9;
+        var cols = 4;
+
         //display results
-        for (int i = 2; i <= 9; i++) {
-            for (int j = 2; j <= 5; j++) {
-                System.out.print(j + " * " + i + " = " + (i * j) + "\t");
-            }
-            System.out.println();
-        }
-        System.out.println();
-        for (int i = 2; i <= 9; i++) {
-            for (int j = 6; j <= 9; j++) {
-                System.out.print(j + " * " + i + " = " + (i * j) + "\t");
+        for (int r = 2; r <= to; r += cols) {
+            for (int i = 2; i <= to; i++) {
+                for (int j = r; j < r + cols && j <= to; j++) {
+                    System.out.print(j + " * " + i + " = " + (i * j) + "\t");
+                }
+                System.out.println();
             }
             System.out.println();
         }
