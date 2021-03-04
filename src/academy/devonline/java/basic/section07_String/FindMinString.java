b10d0317bba749a46b84152cea4f0480f0df10d5
devonline <devonline.academy@gmail.com>
2021-03-04 12:22:38 +0200

    Add FindMinString

---
+++ src/academy/devonline/java/basic/section07_String/FindMinString.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2019. http://devonline.academy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package academy.devonline.java.basic.section07_String;
+
+/**
+ * @author devonline
+ * @link http://devonline.academy/java-basic
+ */
+public class FindMinString {
+    public static void main(String[] args) {
+        // read source data
+        String[] array = {"one", "two", "three", "four", "five"};
+
+        // processing
+        String min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i].compareTo(min) < 0) {
+                min = array[i];
+            }
+        }
+
+        // display results
+        System.out.println(min);
+    }
+}
