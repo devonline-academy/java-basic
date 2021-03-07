ad5bd690d03b5c8eded7c308f52db73fd7b9d805
devonline <devonline.academy@gmail.com>
2021-03-07 12:25:37 +0200

    Add FactorialUsingRecursionVersion2

---
+++ src/academy/devonline/java/basic/section09_recursion/FactorialUsingRecursionVersion2.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2019. http://devonline.academy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package academy.devonline.java.basic.section09_recursion;
+
+/**
+ * @author devonline
+ * @link http://devonline.academy/java-basic
+ */
+public class FactorialUsingRecursionVersion2 {
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+        System.out.println(factorial2(5));
+    }
+
+    private static int factorial(int value) {
+        if (value == 0) {
+            return 1;
+        } else {
+            return value * factorial(value - 1);
+        }
+    }
+
+    private static int factorial2(int value) {
+        return factorial(1, value, 1);
+    }
+
+    private static int factorial(int from, int to, int result) {
+        if (from > to) {
+            return result;
+        } else {
+            return factorial(from + 1, to, result * from);
+        }
+    }
+}
