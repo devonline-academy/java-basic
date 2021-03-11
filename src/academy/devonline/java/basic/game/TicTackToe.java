3c26eacd46270c38a89bb8e467ae528734c52535
devonline <devonline.academy@gmail.com>
2021-03-11 13:24:44 +0200

    Add isDraw

--- src/academy/devonline/java/basic/game/TicTackToe.java
+++ src/academy/devonline/java/basic/game/TicTackToe.java
@@ -191,6 +191,13 @@ public class TicTackToe {
     }
 
     private static boolean isDraw() {
-        return false;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (GAME_TABLE[i][j] == EMPTY) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
